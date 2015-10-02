887e5a00bbd22cc332eb813749d6a004c75257e2
Daniel Martin <consume.noise@gmail.com>
2015-10-02 10:05:29 +0200

    android-x86: HID: multitouch: Ignore invalid reports
    Drop reports with invalid values.
    
    Signed-off-by: Daniel Martin <consume.noise@gmail.com>
    Signed-off-by: lixin <lixin@phoenixos.com>

--- drivers/hid/hid-multitouch.c
+++ drivers/hid/hid-multitouch.c
@@ -618,6 +618,10 @@ static void mt_complete_slot(struct mt_device *td, struct input_dev *input)
 	    td->num_received >= td->num_expected)
 		return;
 
+	if (td->curdata.x == 0xffff && td->curdata.x == td->curdata.y &&
+	    td->curdata.w == 0xffff && td->curdata.w == td->curdata.h)
+		goto inc_num_received;
+
 	if (td->curvalid || (td->mtclass.quirks & MT_QUIRK_ALWAYS_VALID)) {
 		int active;
 		int slotnum = mt_compute_slot(td, input);
@@ -661,6 +665,7 @@ static void mt_complete_slot(struct mt_device *td, struct input_dev *input)
 		}
 	}
 
+inc_num_received:
 	td->num_received++;
 }
 
