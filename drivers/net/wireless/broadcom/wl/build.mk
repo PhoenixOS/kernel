f49faf478f847a755088c59be632edd541490cef
Mauro Rossi <issor.oruam@gmail.com>
2016-06-04 00:12:35 +0200

    net: wireless: broadcom: add support for wl sta driver
    Porting of support for Broadcom wl driver 6.30.223.271.
    
    Since kernel 4.6 wireless drivers folders are organized per vendor.
    So wl folder is moved to drivers/net/wireless/broadcom/.
    
    The linux-recent.patch required specific changes for kernel 4.7
    local paths in {wl,linux-recent}.patch are aligned for simplicity,
    so they can be applied by build.mk with the same command: patch -p1.
    
    The build.mk was originally extracted from build/core/tasks/kernel.mk.

---
+++ drivers/net/wireless/broadcom/wl/build.mk
@@ -0,0 +1,16 @@
+# parts of build/core/tasks/kernel.mk
+
+WL_ENABLED := $(if $(wildcard $(WL_PATH)),$(shell grep ^CONFIG_WL=[my] $(KERNEL_CONFIG_FILE)))
+WL_SRC := $(WL_PATH)/hybrid-v35$(if $(filter x86,$(TARGET_KERNEL_ARCH)),,_64)-nodebug-pcoem-6_30_223_271.tar.gz
+WL_LIB := $(WL_PATH)/lib$(if $(filter x86,$(TARGET_KERNEL_ARCH)),32,64)
+$(WL_SRC):
+	@echo Downloading $(@F)...
+	$(hide) curl https://docs.broadcom.com/docs-and-downloads/docs/linux_sta/$(@F) > $@
+
+$(WL_LIB): $(WL_SRC) $(wildcard $(WL_PATH)/*.patch)
+	$(hide) tar zxf $< -C $(@D) --overwrite -m && \
+		rm -rf $@ && mv $(@D)/lib $@ && \
+		patch -p1 -d $(@D) -i wl.patch && \
+		patch -p1 -d $(@D) -i linux-recent.patch
+
+$(INSTALLED_KERNEL_TARGET): $(if $(WL_ENABLED),$(WL_LIB))
